5626819f6d13f53977a3aa26a2695ca992b6c109
EC2 Default User <ec2-user@ip-172-31-34-48.ap-northeast-2.compute.internal>
2022-03-02 04:58:11 +0000

    20220302 ed

---
+++ Demo_1.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+import numpy as np
+import seaborn as sns
+import spacy
+
+np_array_1 = np.array([[2,3,5],[7,11,13]])
+np_array_2 = np.array([1,4,9])
+
+print(np_array_1 + np_array_2)
+
+print(np_array_1 * np_array_2)
+
+
